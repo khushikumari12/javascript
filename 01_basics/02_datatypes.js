bf982c28c42df72c4caf2a898f25317419d86475
khushikumari12 <102460362+khushikumari12@users.noreply.github.com>
2024-02-20 09:34:32 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//we are using nodejs, not browser as for alert js has diff syntax
+
+//code readability should be high :write codes using good space
+
+let name="khushi"//string
+let age=16
+let isLoggedIn=false 
+
+//PRIMITIVE DATATYPES
+
+//number=> 2 to power 53
+//bigint
+//boolean =>true/false
+//null: stand alone value(empty)
+//undefined => variable value is not defined
+//symbol: use to find uniqueness
+
+
+//OBJECT
+console.log(typeof "khushi")//gives string type
+console.log(typeof null)//but this will give object** IMP **
+console.log(typeof undefined)//give undefined type
+
